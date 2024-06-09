eb0023c4f1ebc1e01ac06c0e79bd4ebc1d29444a
Utkarsh Chakrwarti <76407507+UtkarshChakrwarti@users.noreply.github.com>
2024-06-09 06:17:25 +0530

    renaming
    renaming files

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,18 +1,18 @@
-
 import 'package:flutter/material.dart';
 import 'package:hello_nitr/core/utils/custom_error/custom_error.dart';
 import 'package:hello_nitr/screens/login/login_screen.dart';
+import 'package:hello_nitr/screens/sim_selection/sim_selection_screen.dart';
 import 'package:hello_nitr/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   // Splash Screen
   '/': (_) => const SplashScreen(),
 
-
   //Login Screen
   '/login': (context) => LoginScreen(),
 
-
+  //Sim Selection Screen
+  '/simSelection': (context) => SimSelectionScreen(),
 
   //Custom error page
   '/error': (context) => CustomError(
@@ -22,7 +22,4 @@ final Map<String, WidgetBuilder> appRoutes = {
           stack: StackTrace.fromString('Dummy Stack Trace'),
         ),
       ),
-
-  
-
 };
